a76b6114efdcb7ac1610fececbd8c24ebd707816
Mohammed Waleed <m.elserbeny100@gmail.com>
2026-03-27 12:01:34 +0200

    Finished Login Logic

--- lib/core/networking/api_exceptions.dart
+++ lib/core/networking/api_exceptions.dart
@@ -5,17 +5,17 @@ class ApiExceptions {
   static handelError(DioException error){
     switch(error.type){
       case DioExceptionType.receiveTimeout:
-        return ApiErrors(message: "Connection Timeout");
+        return ApiErrors(message: "Connection Time Out");
       case DioExceptionType.sendTimeout:
-        return ApiErrors(message: "Send Timeout");
+        return ApiErrors(message: "Send Time Out");
       case DioExceptionType.connectionError:
-        return ApiErrors(message: error.message!);
+        return ApiErrors(message: "Connection Error");
       case DioExceptionType.badResponse:
-        return ApiErrors(message: error.message!);
+        return ApiErrors(message: error.response!.data['message']);
       case DioExceptionType.connectionTimeout:
-        return ApiErrors(message: "Connection Timeout");
+        return ApiErrors(message: "Connection Time Out");
       default:
-        return ApiErrors(message: "Something went wrong");
+        return ApiErrors(message: "OPS Something went wrong");
     }
   }
 }
--- lib/features/auth/data/repo/auth_repo.dart
+++ lib/features/auth/data/repo/auth_repo.dart
@@ -1,9 +1,5 @@
-import 'dart:developer';
-
-import 'package:dio/dio.dart';
 import 'package:hungry_app/core/constants/api_constant.dart';
 import 'package:hungry_app/core/networking/api_errors.dart';
-import 'package:hungry_app/core/networking/api_exceptions.dart';
 import 'package:hungry_app/core/networking/api_services.dart';
 import 'package:hungry_app/core/utils/pref_helper.dart';
 import 'package:hungry_app/features/auth/data/models/user_model.dart';
@@ -15,20 +11,19 @@ class AuthRepo {
     required String email,
     required String password,
   }) async {
-      final response = await _apiServices.post(ApiConstants.loginEndPoint, {
-        "email": email,
-        "password": password,
-      });
+    final response = await _apiServices.post(ApiConstants.loginEndPoint, {
+      "email": email,
+      "password": password,
+    });
     try {
       final UserModel user = UserModel.fromJson(response["data"]);
       if (user.token != null) {
         await PrefHelper.saveToken(user.token!);
       }
       return user;
-    } on DioException catch (_) {
-      throw ApiExceptions.handelError(response["message"]);
+    } on ApiErrors catch (e) {
+      throw ApiErrors(message: e.message);
     } catch (e) {
-      log(e.toString());
       throw ApiErrors(message: response.toString());
     }
   }
